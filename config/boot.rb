0201a8db23b70efd8895518e30fa6393d78bba60
ArthurMBMelo <amelo@justworks.com>
2023-04-03 13:39:48 -0300

    still fixing the issue with heroku

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+require 'yaml'
+YAML::ENGINE.yamler = 'syck' if defined?(YAML::ENGINE)
+YAML.load_file(File.expand_path('../database.yml', __FILE__), aliases: true)
